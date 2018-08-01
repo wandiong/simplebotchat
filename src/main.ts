edc07e7154027437dcd7d3ce704fb7a1ca67d2ef
Wandi Ong <nandermind3@gmail.com>
2018-08-01 15:45:13 +0700

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
