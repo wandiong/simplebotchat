58f34eda3a4a3ce00dda7581cefa3802c0f5f4d6
Wandi Ong <nandermind3@gmail.com>
2018-08-01 16:38:44 +0700

    additional
    added a form to put texts
    
    fixed some error

---
+++ src/api/responseDatabase.js
@@ -0,0 +1,14 @@
+function getMessage(messageLarge) {
+  var temp = messageLarge;
+  switch (temp) {
+    case "Hi":
+      return "Sup man";
+      break;
+    case "Bye":
+      return "Bye man, you wont be missed HOHOHOHOHOHOHOHOHOHOHO!";
+      break;
+    default:
+      return "I dont know what are you talking about :( ";
+      break;
+  }
+}
